9ebb3489fdc060dfcff04cab2a1d307b4818ee3b
Ayush Badola <glitchy.56@outlook.com>
2026-07-29 01:53:46 +0530

    Chats reply feature database migration

---
+++ Backend/Migrations/20260728195348_AddReplyToMessageId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReplyToMessageId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReplyToMessageId",
+                table: "ProjectMessages",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReplyToMessageId",
+                table: "Messages",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReplyToMessageId",
+                table: "ProjectMessages");
+
+            migrationBuilder.DropColumn(
+                name: "ReplyToMessageId",
+                table: "Messages");
+        }
+    }
+}
