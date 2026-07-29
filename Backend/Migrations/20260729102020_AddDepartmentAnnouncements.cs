42e4a94ad801583945a92857561280079829c92c
Rachit246-art <connect2rachit882@gmail.com>
2026-07-29 16:05:38 +0530

    chore: Add notifications and announcements migrations

---
+++ Backend/Migrations/20260729102020_AddDepartmentAnnouncements.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentAnnouncements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DepartmentAnnouncements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentAnnouncements", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "SuperAdmins",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$YCPRRhOUmMasj5Iq1Kq89uO/kr7r9FW/AqjpIjs0IL3WuxZ/eRCaa");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentAnnouncements");
+
+            migrationBuilder.UpdateData(
+                table: "SuperAdmins",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$Dy2u0SIodS2sUEHSnSldeuA2YXJFdupjH9txyfX/6C6wH7PspLBt.");
+        }
+    }
+}
