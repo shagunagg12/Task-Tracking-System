c6f729803859ed1fedd9419e4e544927232319d8
kartikgoyal368 <kartikgoyal8091@gmail.com>
2026-08-03 14:35:24 +0530

    Update Backend/Interfaces/IRewardsService.cs

--- Backend/Interfaces/IRewardsService.cs
+++ Backend/Interfaces/IRewardsService.cs
@@ -9,5 +9,8 @@ namespace Backend.Interfaces
         Task<object> GetStatusAsync(ClaimsPrincipal userPrincipal, int userId);
         Task<object> ClaimBonusAsync(ClaimsPrincipal userPrincipal, int userId, ClaimBonusRequest request);
         Task<object> RedeemRewardAsync(ClaimsPrincipal userPrincipal, int userId, RedeemRewardRequest request);
+        Task<object> GetAllRedemptionsAsync(ClaimsPrincipal userPrincipal);
+        Task<object> ApproveRedemptionAsync(ClaimsPrincipal userPrincipal, int redemptionId);
+        Task<object> DeclineRedemptionAsync(ClaimsPrincipal userPrincipal, int redemptionId);
     }
 }
