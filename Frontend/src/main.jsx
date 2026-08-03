bdd04bf957ebb039da616a7e6023e0478743aeee
Rachit246-art <connect2rachit882@gmail.com>
2026-08-03 14:59:00 +0530

    feat: wrap frontend app with GoogleOAuthProvider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="380754775994-nvj1bh4qqarii4ulbpisrnit8orisi2t.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
